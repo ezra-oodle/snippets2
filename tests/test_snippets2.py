ccf9c8299b682633703415caac45bf87b59fc078
Ezra Citron <ezracitron@gmail.com>
2021-12-14 14:44:11 +0000

    first tests

--- tests/test_snippets2.py
+++ tests/test_snippets2.py
@@ -1,1 +1,9 @@
 from snippets2 import snippets2
+import pytest
+
+@pytest.fixture
+def teal_colour():
+    return ("#0AD2A0")
+
+def test_Colours(my_colour):
+    assert snippets2.Colours().oodle_teal == teal_colour
